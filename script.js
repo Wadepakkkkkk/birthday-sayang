b74bd1395e9abfbce81b78fb563360370b9b1234
Wadepakkkkkk <chehanif61@gmail.com>
2026-08-26 14:33:52 +0800

    Add continue button functionality to scene transitions

--- script.js
+++ script.js
@@ -6,3 +6,10 @@ startButton.addEventListener("click", function () {
   startScreen.classList.add("hidden");
   scene2.classList.remove("hidden");
 });
+const continueButton = document.getElementById("continue-btn");
+const scene3 = document.getElementById("scene-3");
+
+continueButton.addEventListener("click", function () {
+  scene2.classList.add("hidden");
+  scene3.classList.remove("hidden");
+});
